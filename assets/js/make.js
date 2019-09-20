87a57628c42c911ed7b6ec5f923d0e1a46f1f1c6
murphya14 <ubuntu@ip-172-31-90-201.ec2.internal>
2019-09-20 10:39:18 +0000

    Added a modal popup using jQuery

---
+++ assets/js/make.js
@@ -0,0 +1,10 @@
+
+$(".open").on("click", function() {
+  $(".popup-overlay, .popup-content").addClass("active");
+  
+});
+
+//removes the "active" class to .popup and .popup-content when the "Close" button is clicked 
+$(".close, .popup-overlay").on("click", function() {
+  $(".popup-overlay, .popup-content").removeClass("active");
+});
